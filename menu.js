51091ca53c4f0d54359aebce7195fabece1976c9
Piotr Sitarz <piotrsitarz@onet.eu>
2017-04-10 19:35:55 +0200

    menu

--- menu.js
+++ menu.js
@@ -1,14 +1,7 @@
-$(document).ready(function(){
-    $("#menu p").click(function() {
-        $(this).css("color",'pink');
-    });
-});
-
-
 
 $(document).ready(function(){
     $("#menu p").click(function(){
-        $("navbar").toggleClass("navbar", 10000);
+        $("navbar").toggleClass("navbar");
     });
 });
 
